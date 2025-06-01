86626518736027652bb7fa815d49f69e0fa3b2e3
Mattias Wallin <mattias@klingawallin.se>
2025-06-01 08:32:50 +0200

    feat(cli): move -d and -m to required group
    Move the arguments --directory/-d and --module/-m into a required
    argument group, so that at least one have to be specified. This makes
    error reporting and --help nicer.

--- rasn-compiler/src/cli.rs
+++ rasn-compiler/src/cli.rs
@@ -9,14 +9,8 @@ use walkdir::WalkDir;
 #[derive(clap::Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct CompilerArgs {
-    /// Specify a directory for the compiler to search for ASN1 modules.
-    /// The compiler will search recursively for `.asn` and `.asn1` files
-    #[arg(short, long)]
-    directory: Option<PathBuf>,
-
-    /// Add an ASN1 module by path. Multiple modules can be added by appending "-m PATH_TO_MODULE"
-    #[arg(short, long = "module", num_args(0..))]
-    module_files: Vec<PathBuf>,
+    #[clap(flatten)]
+    source: SourceArgsGroup,
 
     /// Set the output path for the generated rust module
     #[arg(short, long, default_value = ".")]
@@ -29,15 +23,28 @@ pub struct CompilerArgs {
     backend: String,
 }
 
+#[derive(clap::Args, Debug)]
+#[group(required = true, multiple = true)]
+pub struct SourceArgsGroup {
+    /// Specify a directory for the compiler to search for ASN1 modules.
+    /// The compiler will search recursively for `.asn` and `.asn1` files
+    #[arg(short, long)]
+    directory: Option<PathBuf>,
+
+    /// Add an ASN1 module by path. Multiple modules can be added by appending "-m PATH_TO_MODULE"
+    #[arg(short, long = "module", num_args(0..))]
+    module_files: Vec<PathBuf>,
+}
+
 impl CompilerArgs {
     pub fn drive() {
         let args = CompilerArgs::parse();
 
         // Read module paths
-        let mut modules = args.module_files;
+        let mut modules = args.source.module_files;
 
         // Scan directory, if given
-        if let Some(dir) = args.directory {
+        if let Some(dir) = &args.source.directory {
             for entry in WalkDir::new(dir)
                 .follow_links(true)
                 .into_iter()
@@ -53,11 +60,8 @@ impl CompilerArgs {
         }
 
         if modules.is_empty() {
-            panic!(
-                "{}",
-                "Please provide either a valid path to a module or to a directory containing modules."
-                .red()
-            )
+            println!("{}", "No modules found".red());
+            return;
         }
 
         let results = if args.backend == "typescript" {
